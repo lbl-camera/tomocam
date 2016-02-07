8c441366aa1d27455d5846c4253d8d74ddce9d1e
Dinesh Kumar <dkumar@lbl.gov>
2016-02-06 17:34:47 -0800

    place holder for polargrid

---
+++ cuda/polargrid.cc
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <Python.h>
+
+#include "pyGnufft.h"
+
+PyObject *cPolarGrid(PyObject *self, PyObject *prhs) {
+    fprintf(stderr,"Not implemented yet\n");
+    return NULL;
+}
