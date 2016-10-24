f69425eb9347adc70530efa71d04c53640ed77e2
svvenkatakrishnan_lbl <svvenkatakrishnan@lbl.gov>
2016-10-24 13:43:00 -0700

    Fixing bugs and cleaning up benchmarking code

--- python_2D/XT_ForwardModel.py
+++ python_2D/XT_ForwardModel.py
@@ -88,10 +88,11 @@ def init_nufft_params(sino,geom):
     params['gkblut'] = afnp.array(np.single(kblut))
     params['det_grid'] = np.array(np.reshape(np.arange(0,sino['Ns']),(sino['Ns'],1)))
 
-    #####Generate Ram-Lak/ShepLogan like kernel#########
+    #####Generate Ram-Lak/ShepLogan like filter kernel#########
     temp_r = np.linspace(-1,1,Ns)
     kernel = (Ns)*np.fabs(temp_r)*np.sinc(temp_r/2)
-    temp_mask=np.ones(Ns);temp_mask[0:Ns/2-100]=0;temp_mask[3*Ns/4-100:]=0
+    temp_mask=np.ones(Ns);
+    temp_mask[0:Ns/4]=0;temp_mask[3*Ns/4:]=0
     params['giDq']=afnp.array(kernel*temp_mask,dtype=afnp.complex64)
     
     temp = afnp.array((-1)**params['det_grid'],dtype=afnp.float32)
--- python_2D/benchmark_Gridrec.py
+++ python_2D/benchmark_Gridrec.py
@@ -9,12 +9,15 @@ import matplotlib.pyplot as plt
 from XT_ForwardModel import forward_project, init_nufft_params, back_project
 from XT_Common import padmat,padmat_v2
 
-
+##Input parameters
 fpath='/home/svvenkatakrishnan/data/20130807_234356_OIM121R_SAXS_5x_Full.mat'
-start_slice=500
-end_slice=561
+start_slice=400
+end_slice=463
 pad_size = 3200 #Size of image for Fourier transform
+sino_center = 1294
 
+
+#Read and re-organize data
 f = h5py.File(fpath)
 norm_data=f['norm_data']
 norm_data=np.transpose(norm_data,(2,1,0))
@@ -23,50 +26,60 @@ norm_data=norm_data[start_slice:end_slice] #Crop data set
 num_slice =  norm_data.shape[0]
 num_angles= norm_data.shape[2]
 im_size =  norm_data.shape[1] #A n X n X n volume
-sino_center = 1294
 slice_idx = num_slice/2
 ang = tomopy.angles(num_angles+1) # Generate uniformly spaced tilt angles.
 ang=ang[:-1]
 
+#Initialize structures for NUFFT
 sino={}
 geom={}
-sino['Ns'] = 3200#im_size*2 #Sinogram size after padding
+sino['Ns'] = pad_size #Sinogram size after padding
 sino['Ns_orig'] = im_size #size of original sinogram
 sino['center'] = sino_center + (sino['Ns']/2 - sino['Ns_orig']/2)  #for padded sinogram
 sino['angles'] = ang
 sino['filter'] = 1 #Paramter to control strength of FBP filter normalized to [0,1]
 
+#Initialize NUFFT parameters
 params = init_nufft_params(sino,geom)
 
 #norm_data=afnp.array(norm_data[:40],dtype=afnp.complex64)
 #temp_mat=afnp.array(np.zeros((sino['Ns'],num_angles)),dtype=afnp.complex64)
 
+rec_nufft = afnp.zeros((num_slice,sino['Ns'],sino['Ns']),dtype=afnp.complex64)
+
 t=time.time()
 #loop over all slices
-for i in range(1,num_slice):
+for i in range(0,num_slice):
   #pad data array and move it to GPU 
   Ax = afnp.array(padmat(norm_data[i],np.array([sino['Ns'],num_angles]),0),dtype=afnp.complex64)
 #  Ax = padmat_v2(norm_data[i],np.array([sino['Ns'],num_angles]),0,temp_mat)
 #  temp_mat=temp_mat*0
-  #filtered back-projection
-  y = back_project(Ax,params)
+  #filtered back-projection 
+  rec_nufft[i] = back_project(Ax,params)
   
 elapsed_time = (time.time()-t)
 print('Time for NUFFT Back-proj of %d slices : %f' % (num_slice,elapsed_time))
 
+#Rescale result to match tomopy
+rec_nufft=rec_nufft/(2*np.pi*np.pi*sino['Ns'])
+temp_index = slice(sino['Ns']/2-sino['Ns_orig']/2,sino['Ns']/2+sino['Ns_orig']/2)
 
-plt.figure();plt.imshow(np.abs(y),cmap='gray');plt.colorbar();plt.title('Reconstructed slice using FastNUFFT');plt.draw();
+#Move to CPU 
+rec_nufft=np.array(rec_nufft)
+print(rec_nufft.shape)
+
+plt.figure();plt.imshow(np.abs(rec_nufft[slice_idx,temp_index,temp_index]),cmap='gray');plt.colorbar();plt.title('Reconstructed slice using FastNUFFT');plt.draw();
 
 ##Tomopy
 data = np.transpose(norm_data,(2,0,1))
 data.astype(np.float32)
 #np.zeros((1024, 50, 2560), dtype=np.float32)
 t = time.time()
-rec = tomopy.recon(data, ang,center=sino_center, algorithm='gridrec', ncore=4)
+rec_tomopy = tomopy.recon(data, ang,center=sino_center, algorithm='gridrec', ncore=4)
 print('Time for tomopy gridrec of %d slices : %f' % (data.shape[1],time.time() - t))
 
-print(rec.shape)
+print(rec_tomopy.shape)
 
-plt.figure();plt.imshow(np.flipud(np.abs(rec[-1])),cmap='gray');plt.colorbar();plt.title('Reconstructed slice using TomoPy');plt.draw();
+plt.figure();plt.imshow(np.flipud(np.abs(rec_tomopy[slice_idx])),cmap='gray');plt.colorbar();plt.title('Reconstructed slice using TomoPy');plt.draw();
 
 plt.show()
