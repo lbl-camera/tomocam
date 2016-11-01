7d4ed4377feda9ec644d66708a130067ea071d5d
svvenkatakrishnan_lbl <svvenkatakrishnan@lbl.gov>
2016-11-01 15:58:14 -0700

    Adding gridrec to tomoCam

--- python_2D/reconstruct_gridrec.py
+++ python_2D/reconstruct_gridrec.py
@@ -57,51 +57,7 @@ def main():
         input_params['gpu_device']=inputs['gpu_device']
         input_params['fbp_filter_param']=inputs['fbp_filter_param']
         input_params['oversamp_factor']=oversamp_factor
-        rec_nufft_final=gpuGridrec(tomo,theta,inputs['rot_center'],input_params)        
-        
-        #print('Starting GPU NUFFT recon')
-        #allocate space for final answer 
-        #rec_nufft_final=np.zeros((num_slice,sino['Ns_orig'],sino['Ns_orig']),dtype=np.float32)
-        #af.set_device(inputs['gpu_device']) #Set the device number for gpu based code
-        #Change tomopy format
-        #new_tomo=np.transpose(tomo,(1,2,0))
-        #im_size =  new_tomo.shape[1]
-        #Initialize structures for NUFFT
-        #sino={}
-        #geom={}
-        #sino['Ns'] = pad_size #Sinogram size after padding
-        #sino['Ns_orig'] = im_size #size of original sinogram
-        #sino['center'] = inputs['rot_center'] + (sino['Ns']/2 - sino['Ns_orig']/2)  #for padded sinogram
-        #sino['angles'] = theta
-        #sino['filter'] = fbp_filter_param #Paramter to control strength of FBP filter normalized to [0,1]
-
-        #Initialize NUFFT parameters
-        #params = init_nufft_params(sino,geom)
-
-        #rec_nufft = afnp.zeros((num_slice/2,sino['Ns_orig'],sino['Ns_orig']),dtype=afnp.complex64)
-        #Ax = afnp.zeros((sino['Ns'],num_angles),dtype=afnp.complex64)
-        #pad_idx = slice(sino['Ns']/2-sino['Ns_orig']/2,sino['Ns']/2+sino['Ns_orig']/2)
-        
-        #t=time.time()
-        #Move all data to GPU
-        #slice_1=slice(0,num_slice,2)
-        #slice_2=slice(1,num_slice,2)
-        #gdata=afnp.array(new_tomo[slice_1]+1j*new_tomo[slice_2],dtype=afnp.complex64)
-        #x_recon = afnp.zeros((sino['Ns'],sino['Ns']),dtype=afnp.complex64)
-        #loop over all slices
-        #for i in range(0,num_slice/2):
-        #  Ax[pad_idx,:]=gdata[i]
-          #filtered back-projection 
-        #  rec_nufft[i] = (back_project(Ax,params))[pad_idx,pad_idx]
-
-#        elapsed_time = (time.time()-t)
-#        print('Time for NUFFT Back-proj of %d slices : %f' % (num_slice,elapsed_time))
-
-        #Move to CPU
-        #Rescale result to match tomopy
-#        rec_nufft=np.array(rec_nufft,dtype=np.complex64)*nufft_scaling
-#        rec_nufft_final[slice_1]=np.array(rec_nufft.real,dtype=np.float32)
-#        rec_nufft_final[slice_2]=np.array(rec_nufft.imag,dtype=np.float32)
+        rec_nufft_final=gpuGridrec(tomo,theta,inputs['rot_center'],input_params)                
 
         ##################TomoPy Recon#####################
 #        print('Recon - tomopy GridRec')
@@ -116,7 +72,6 @@ def main():
 #       rec_astra = tomopy.recon(tomo, theta, center=inputs['rot_center'], algorithm=tomopy.astra, options=options)
 #       elapsed_time = (time.time()-t)
 #       print('Time for reconstucting using Tomopy Astra of %d slices : %f' % (num_slice,elapsed_time))
-
 
         
 #        fig = plt.figure()
--- python_2D/reconstruct_sirt.py
+++ python_2D/reconstruct_sirt.py
@@ -11,7 +11,8 @@ import pyqtgraph as pg
 
 from XT_argsParser import bl832inputs_parser
 from normalize import normalize_bo
-from XT_ForwardModel import forward_project, init_nufft_params, back_project
+from tomoCam import gpuSIRT
+#from XT_ForwardModel import forward_project, init_nufft_params, back_project
 
 def main():
 
@@ -45,70 +46,75 @@ def main():
         
 
         ################## GPU SIRT() ######################
-        print('Starting GPU SIRT recon')
+        input_params={}
+        input_params['gpu_device']=inputs['gpu_device']
+        input_params['oversamp_factor']=oversamp_factor
+        input_params['num_iter']=num_iter
+        rec_sirt_final = gpuSIRT(tomo,theta,inputs['rot_center'],input_params)
 
-        new_tomo=np.transpose(tomo,(1,2,0))
-        im_size =  new_tomo.shape[1]
+#        print('Starting GPU SIRT recon')
+#        new_tomo=np.transpose(tomo,(1,2,0))
+#        im_size =  new_tomo.shape[1]
         #Initialize structures for NUFFT
-        sino={}
-        geom={}
-        sino['Ns'] = pad_size #Sinogram size after padding
-        sino['Ns_orig'] = im_size #size of original sinogram
-        sino['center'] = inputs['rot_center'] + (sino['Ns']/2 - sino['Ns_orig']/2)  #for padded sinogram
-        sino['angles'] = theta
+#        sino={}
+#        geom={}
+#        sino['Ns'] = pad_size #Sinogram size after padding
+#        sino['Ns_orig'] = im_size #size of original sinogram
+#        sino['center'] = inputs['rot_center'] + (sino['Ns']/2 - sino['Ns_orig']/2)  #for padded sinogram
+#        sino['angles'] = theta
 
         #Initialize NUFFT parameters
-        params = init_nufft_params(sino,geom)
+#        params = init_nufft_params(sino,geom)
 
-        temp_y = afnp.zeros((sino['Ns'],num_angles),dtype=afnp.complex64)
-        temp_x = afnp.zeros((sino['Ns'],sino['Ns']),dtype=afnp.complex64)
-        x_recon  = afnp.zeros((num_slice/2,sino['Ns_orig'],sino['Ns_orig']),dtype=afnp.complex64) 
-        pad_idx = slice(sino['Ns']/2-sino['Ns_orig']/2,sino['Ns']/2+sino['Ns_orig']/2)
+#        temp_y = afnp.zeros((sino['Ns'],num_angles),dtype=afnp.complex64)
+#        temp_x = afnp.zeros((sino['Ns'],sino['Ns']),dtype=afnp.complex64)
+#        x_recon  = afnp.zeros((num_slice/2,sino['Ns_orig'],sino['Ns_orig']),dtype=afnp.complex64) 
+#        pad_idx = slice(sino['Ns']/2-sino['Ns_orig']/2,sino['Ns']/2+sino['Ns_orig']/2)
 
 
         #Pre-compute diagonal scaling matrices ; one the same size as the image and the other the same as data
         #initialize an image of all ones
-        x_ones= afnp.ones((sino['Ns_orig'],sino['Ns_orig']),dtype=afnp.complex64)
-        temp_x[pad_idx,pad_idx]=x_ones
-        temp_proj=forward_project(temp_x,params)*(sino['Ns']*afnp.pi/2)
-        R = 1/afnp.abs(temp_proj)#(math.pi/2)*sino['Ns']*
-        R[afnp.isnan(R)]=0
-        R[afnp.isinf(R)]=0
-        R=afnp.array(R,dtype=afnp.complex64)
+#        x_ones= afnp.ones((sino['Ns_orig'],sino['Ns_orig']),dtype=afnp.complex64)
+#        temp_x[pad_idx,pad_idx]=x_ones
+#        temp_proj=forward_project(temp_x,params)*(sino['Ns']*afnp.pi/2)
+#        R = 1/afnp.abs(temp_proj)#(math.pi/2)*sino['Ns']*
+#        R[afnp.isnan(R)]=0
+#        R[afnp.isinf(R)]=0
+#        R=afnp.array(R,dtype=afnp.complex64)
 
         #Initialize a sinogram of all ones
-        y_ones=afnp.ones((sino['Ns_orig'],num_angles),dtype=afnp.complex64)
-        temp_y[pad_idx]=y_ones
-        temp_backproj=back_project(temp_y,params)*nufft_scaling/2
-        C = 1/(afnp.abs(temp_backproj))
-        C[afnp.isnan(C)]=0
-        C[afnp.isinf(C)]=0
-        C=afnp.array(C,dtype=afnp.complex64)
+#        y_ones=afnp.ones((sino['Ns_orig'],num_angles),dtype=afnp.complex64)
+#        temp_y[pad_idx]=y_ones
+#        temp_backproj=back_project(temp_y,params)*nufft_scaling/2
+#        C = 1/(afnp.abs(temp_backproj))
+#        C[afnp.isnan(C)]=0
+#        C[afnp.isinf(C)]=0
+#        C=afnp.array(C,dtype=afnp.complex64)
         
-        t=time.time()
+#        t=time.time()
         #Move all data to GPU
-        slice_1=slice(0,num_slice,2)
-        slice_2=slice(1,num_slice,2)
-        gdata=afnp.array(new_tomo[slice_1]+1j*new_tomo[slice_2],dtype=afnp.complex64)
+#        slice_1=slice(0,num_slice,2)
+#        slice_2=slice(1,num_slice,2)
+#        gdata=afnp.array(new_tomo[slice_1]+1j*new_tomo[slice_2],dtype=afnp.complex64)
         
         #loop over all slices
-        for i in range(0,num_slice/2):
-          for iter_num in range(1,num_iter+1):
-            #filtered back-projection
-            temp_x[pad_idx,pad_idx]=x_recon[i]
-            Ax = (math.pi/2)*sino['Ns']*forward_project(temp_x,params)
-            temp_y[pad_idx]=gdata[i]
-            x_recon[i] = x_recon[i]+(C*back_project(R*(temp_y-Ax),params)*nufft_scaling/2)[pad_idx,pad_idx]
+#        for i in range(0,num_slice/2):
+#          for iter_num in range(1,num_iter+1):
+#            #filtered back-projection
+#            temp_x[pad_idx,pad_idx]=x_recon[i]
+#            Ax = (math.pi/2)*sino['Ns']*forward_project(temp_x,params)
+#            temp_y[pad_idx]=gdata[i]
+#            x_recon[i] = x_recon[i]+(C*back_project(R*(temp_y-Ax),params)*nufft_scaling/2)[pad_idx,pad_idx]
 
-        elapsed_time = (time.time()-t)
-        print('Time for SIRT recon of %d slices : %f' % (num_slice,elapsed_time))
+#       elapsed_time = (time.time()-t)
+#        print('Time for SIRT recon of %d slices : %f' % (num_slice,elapsed_time))
 
         #Move to CPU
         #Rescale result to match tomopy
-        rec_sirt=np.array(x_recon,dtype=np.complex64)
-        rec_sirt_final=np.zeros((num_slice,sino['Ns_orig'],sino['Ns_orig']),dtype=np.float32)
-        rec_sirt_final[slice_1]=np.array(rec_sirt.real,dtype=np.float32)
-        rec_sirt_final[slice_2]=np.array(rec_sirt.imag,dtype=np.float32)
+#        rec_sirt=np.array(x_recon,dtype=np.complex64)
+#        rec_sirt_final=np.zeros((num_slice,sino['Ns_orig'],sino['Ns_orig']),dtype=np.float32)
+#        rec_sirt_final[slice_1]=np.array(rec_sirt.real,dtype=np.float32)
+#        rec_sirt_final[slice_2]=np.array(rec_sirt.imag,dtype=np.float32)
 
 #        fig = plt.figure()
 #        sirt_camTomo = rec_sirt_final[0] 
@@ -122,7 +128,7 @@ def main():
 
         ##################TomoPy Recon#####################
         print('Recon - tomopy ASTRA-SIRT')
-        options = {'proj_type':'cuda', 'method':'SIRT_CUDA', 'num_iter':15}
+        options = {'proj_type':'cuda', 'method':'SIRT_CUDA', 'num_iter':num_iter}
         t=time.time()
         rec_tomopy = tomopy.recon(tomo, theta, center=inputs['rot_center'],algorithm=tomopy.astra,options=options)#emission=False)
         elapsed_time = (time.time()-t)
