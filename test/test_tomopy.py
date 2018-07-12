0391562844425e900f459a77af668ccdd6fc6dfc
Dinesh Kumar <dkumar@lbl.gov>
2018-07-12 10:28:58 -0700

    1. Removed unrelated files from the test subdir 2. cleaned up tomopy test

--- test/test_tomopy.py
+++ test/test_tomopy.py
@@ -2,33 +2,24 @@
 
 from __future__ import print_function, division
 import matplotlib.pyplot as plt
-from skimage.io import imread
-from skimage import data_dir
-from skimage.transform import radon, rescale, iradon
 import tomopy
-from normalize import normalize_bo
+import os
+import dxchange as dx
+from tomopy.prep.normalize import normalize
 
-dataset='/home/svvenkatakrishnan/data/20130807_234356_OIM121R_SAXS_5x.h5'
-algorithm='sirt'
-tomo, flats, darks, floc = tomopy.read_als_832h5(dataset,sino=(1000, 1003, 1))
-print('Data read complete')
+dataset=os.path.join(os.environ['HOME'], 'data', '20130807_234356_OIM121R_SAXS_5x.h5')
+tomo, flats, darks, floc = dx.read_als_832h5(dataset,sino=(1000, 1003, 1))
 
 print('Displaying  sinogram')
-imgplot = plt.imshow(tomo[:,0,:])
+plt.imshow(tomo[:,0,:])
+plt.show()
 
-print('Generating angles')
 theta = tomopy.angles(tomo.shape[0])
-
-print('Normalization')
-tomo = normalize_bo(tomo, flats, darks,20)
-
-print('Ring removal')
+tomo = normalize(tomo, flats, darks)
 tomo = tomopy.remove_stripe_fw(tomo)
-
-print('Recon')
-rec = tomopy.recon(tomo, theta, center=1294,algorithm='osem',emission=False)
-
-print('Masking')
+rec = tomopy.recon(tomo, theta, center=1294,algorithm='gridrec')
 rec = tomopy.circ_mask(rec, 0)
 
-tomopy.write_tiff_stack(rec, 'test.tiff')
+for i in range(3):
+    plt.imshow(rec[i])
+    plt.show()
