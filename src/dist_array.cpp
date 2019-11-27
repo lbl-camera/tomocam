ee10581924e3c4044b22ac056d34bdca707b8bac
Dinesh Kumar <dkumar@lbl.gov>
2019-11-26 16:17:11 -0800

    Changed all dimension type to int to match with cufft

--- src/dist_array.cpp
+++ src/dist_array.cpp
@@ -1,21 +1,22 @@
 /* -------------------------------------------------------------------------------
-* Tomocam Copyright (c) 2018
-*
-* The Regents of the University of California, through Lawrence Berkeley National
-* Laboratory (subject to receipt of any required approvals from the U.S. Dept. of
-*  Energy). All rights reserved.
-*
-* If you have questions about your rights to use or distribute this software,
-* please contact Berkeley Lab's Innovation & Partnerships Office at IPO@lbl.gov.
-*
-* NOTICE. This Software was developed under funding from the U.S. Department of
-* Energy and the U.S. Government consequently retains certain rights. As such, the
-* U.S. Government has been granted for itself and others acting on its behalf a
-* paid-up, nonexclusive, irrevocable, worldwide license in the Software to
-* reproduce, distribute copies to the public, prepare derivative works, and
-* perform publicly and display publicly, and to permit other to do so.
-*---------------------------------------------------------------------------------
-*/
+ * Tomocam Copyright (c) 2018
+ *
+ * The Regents of the University of California, through Lawrence Berkeley
+ *National Laboratory (subject to receipt of any required approvals from the
+ *U.S. Dept. of Energy). All rights reserved.
+ *
+ * If you have questions about your rights to use or distribute this software,
+ * please contact Berkeley Lab's Innovation & Partnerships Office at
+ *IPO@lbl.gov.
+ *
+ * NOTICE. This Software was developed under funding from the U.S. Department of
+ * Energy and the U.S. Government consequently retains certain rights. As such,
+ *the U.S. Government has been granted for itself and others acting on its
+ *behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software
+ *to reproduce, distribute copies to the public, prepare derivative works, and
+ * perform publicly and display publicly, and to permit other to do so.
+ *---------------------------------------------------------------------------------
+ */
 
 #include <cuda.h>
 #include <cuda_runtime.h>
@@ -31,10 +32,10 @@ namespace tomocam {
 
         // allocate memory for the array
         size_t buffersize = sizeof(T) * size_;
-        if (unified_) 
-            cudaMallocManaged((void **) &buffer_, buffersize);
-        else 
-            cudaMallocHost((void **) &buffer_, buffersize);
+        if (unified_)
+            cudaMallocManaged((void **)&buffer_, buffersize);
+        else
+            cudaMallocHost((void **)&buffer_, buffersize);
     }
 
     template <typename T>
@@ -44,18 +45,22 @@ namespace tomocam {
         if (buffer_) cudaFree(buffer_);
     }
 
-    template <typename T> 
-    std::vector<Partition> DArray<T>::create_partitions(int n_partitions, bool with_halo) {
-        int n_slices = dims_.x / n_partitions;
-        int n_extra = dims_.x % n_partitions;
-        std::vector<Partition> table;
+    template <typename T>
+    std::vector<Partition<T>> DArray<T>::create_partitions(int n_partitions) {
+        dim3_t d        = dims_;
+        int    n_slices = dims_.x / n_partitions;
+        int    n_extra  = dims_.x % n_partitions;
 
-        dim3_t d = dims_;
+        // vector to hold the partitions
+        std::vector<Partition<T>> table;
+
         int offset = 0;
         for (int i = 0; i < n_partitions; i++) {
-            if (i < n_extra) d.x = n_slices+1;
-            else d.x = n_slices;
-            table.push_back(new Partition(d, buffer_ + offset));
+            if (i < n_extra)
+                d.x = n_slices + 1;
+            else
+                d.x = n_slices;
+            table.push_back(Partition<T>(d, buffer_ + offset));
             offset += d.x;
         }
         return table;
--- src/dist_array.h
+++ src/dist_array.h
@@ -1,21 +1,22 @@
 /* -------------------------------------------------------------------------------
-* Tomocam Copyright (c) 2018
-*
-* The Regents of the University of California, through Lawrence Berkeley National
-* Laboratory (subject to receipt of any required approvals from the U.S. Dept. of
-*  Energy). All rights reserved.
-*
-* If you have questions about your rights to use or distribute this software,
-* please contact Berkeley Lab's Innovation & Partnerships Office at IPO@lbl.gov.
-*
-* NOTICE. This Software was developed under funding from the U.S. Department of
-* Energy and the U.S. Government consequently retains certain rights. As such, the
-* U.S. Government has been granted for itself and others acting on its behalf a
-* paid-up, nonexclusive, irrevocable, worldwide license in the Software to
-* reproduce, distribute copies to the public, prepare derivative works, and
-* perform publicly and display publicly, and to permit other to do so.
-*---------------------------------------------------------------------------------
-*/
+ * Tomocam Copyright (c) 2018
+ *
+ * The Regents of the University of California, through Lawrence Berkeley
+ *National Laboratory (subject to receipt of any required approvals from the
+ *U.S. Dept. of Energy). All rights reserved.
+ *
+ * If you have questions about your rights to use or distribute this software,
+ * please contact Berkeley Lab's Innovation & Partnerships Office at
+ *IPO@lbl.gov.
+ *
+ * NOTICE. This Software was developed under funding from the U.S. Department of
+ * Energy and the U.S. Government consequently retains certain rights. As such,
+ *the U.S. Government has been granted for itself and others acting on its
+ *behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software
+ *to reproduce, distribute copies to the public, prepare derivative works, and
+ * perform publicly and display publicly, and to permit other to do so.
+ *---------------------------------------------------------------------------------
+ */
 
 #ifndef TOMOCAM_DISTARRAY__H
 #define TOMOCAM_DISTARRAY__H
@@ -25,39 +26,36 @@
 namespace tomocam {
 
     struct dim3_t {
-        unsigned x, y, z;
-        dim3_t(const unsigned *d) : x(d[0]), y(d[1]), z(d[2]) {}
-        dim3_t(const unsigned d0, const unsigned d1, const unsigned d2)
+        int x, y, z;
+        dim3_t(const int *d) : x(d[0]), y(d[1]), z(d[2]) {}
+        dim3_t(const int d0, const int d1, const int d2)
             : x(d0), y(d1), z(d2) {}
     };
 
     template <typename T>
     class Partition {
       private:
-        dim3_t   dims_;
-        T *      first_;
-        unsigned size_;
+        dim3_t dims_;
+        T *    first_;
+        int    size_;
 
       public:
-        Partition()
-            : dims_({0, 0, 0}), first_(nullptr), size_(0),
-              device_id_(-1) {}
-        Partition(dim3_t d, T *pos) : dims_(d), first_(pos)  {
-            size_      = dims_.x * dims_.y * dims_.z;
-            device_id_ = -1;
+        Partition() : dims_({0, 0, 0}), first_(nullptr), size_(0) {}
+
+        Partition(dim3_t d, T *pos) : dims_(d), first_(pos) {
+            size_ = dims_.x * dims_.y * dims_.z;
         }
 
         dim3_t dims() const { return dims_; }
-        unsigned size() const { return size_; }
-        T *      begin() { return first_; }
-        
+        int    size() const { return size_; }
+        T *    begin() { return first_; }
     };
 
     template <typename T>
     class DArray {
       private:
         dim3_t dims_;    ///< [Slices, Rows, Colums]
-        size_t size_;    ///< Size of the alloated array
+        int    size_;    ///< Size of the alloated array
         T *    buffer_;  ///< Pointer to data buffer
         bool   unified_; ///< Flag for unified memory support
 
@@ -68,8 +66,8 @@ namespace tomocam {
         }
 
       public:
-        DArray(unsigned *);
-        DArray(unsigned, unsigned, unsigned);
+        DArray(int *);
+        DArray(int, int, int);
         DArray(dim3_t);
         ~DArray();
 
@@ -80,14 +78,14 @@ namespace tomocam {
         DArray operator=(DArray &&) = delete;
 
         // setup partitioning of array along slowest axis
-        void create_partitions(unsigned);
+        std::vector<Partition<T>> create_partitions(int);
 
         // getters
         /// dimensions of the array
         dim3_t dims() const { return dims_; };
-        size_t slices() const { return dims_.x; }
-        size_t rows() const { return dims_.y; }
-        size_t cols() const {return dims_.z}
+        int slices() const { return dims_.x; }
+        int rows() const { return dims_.y; }
+        int cols() const { return dims_.z; }
 
         // indexing
         T &operator()(int);
@@ -99,7 +97,6 @@ namespace tomocam {
 
         /// Expose the alloaated memoy pointer
         T *data() { return buffer_; }
-
     };
 } // namespace tomocam
 
