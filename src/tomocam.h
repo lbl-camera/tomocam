0aba5c31325d5c7adfc11dec0940dbe7605814e9
Dinesh Kumar <dkumar@lbl.gov>
2020-03-31 16:41:15 -0700

    Added norm and axpy functions to header

--- src/tomocam.h
+++ src/tomocam.h
@@ -1,12 +1,7 @@
 #ifndef TOMOCAM__H
 #define TOMOCAM__H
 
-#include <cuda.h>
-#include <cuda_runtime.h>
-
-#include "dev_array.h"
 #include "dist_array.h"
-#include "types.h"
 
 namespace tomocam {
 
@@ -23,8 +18,10 @@ namespace tomocam {
     void add_total_var(DArray<float> &, DArray<float> &, float , float);
 
     // TODO document
-    float lipschitz();
+    void axpy(float, DArray<float> &, DArray<float> &);
 
+    // TODO document
+    float norm2(DArray<float> &);
 } // namespace tomocam
 
 #endif // TOMOCAM__H
