fc8ba13687b879e6ff94e695698f2a99c6ddf3d4
Dinesh Kumar <dkumar@lbl.gov>
2020-04-06 21:20:00 -0700

    better way to ensure compatibility between python and c++ calls

--- src/pyTomocam.cpp
+++ src/pyTomocam.cpp
@@ -20,7 +20,29 @@
 #include "types.h"
 #include "dist_array.h"
 #include "tomocam.h"
+#include "machine.h"
 
+template <typename T>
+inline T * getPtr(np_array_t<T> array) {
+    return (T *) array.request().ptr;
+}
+
+void radon_wrapper(tomocam::DArray<float> &arg1, tomocam::DArray<float> &arg2, np_array_t<float> angs, double cen, double oversamp) {
+    tomocam::radon(arg1, arg2, getPtr<float>(angs), (float) cen, (float) oversamp);
+}
+        
+void iradon_wrapper(tomocam::DArray<float> &arg1, tomocam::DArray<float> &arg2, np_array_t<float> angs, double cen, double oversamp) {
+    tomocam::iradon(arg1, arg2, getPtr<float>(angs), (float) cen, (float) oversamp);
+}
+        
+void gradient_wrapper(tomocam::DArray<float> &arg1, tomocam::DArray<float> &arg2, np_array_t<float> angs, double cen, double oversamp) {
+    tomocam::gradient(arg1, arg2, getPtr<float>(angs), (float) cen, (float) oversamp);
+}
+        
+void tv_wrapper(tomocam::DArray<float> &arg1, tomocam::DArray<float> &arg2, double p, double s) {
+    tomocam::add_total_var(arg1, arg2, (float) p, (float) s);
+}
+ 
 
 /* setup methods table */
 PYBIND11_MODULE(cTomocam, m) {
@@ -31,18 +53,37 @@ PYBIND11_MODULE(cTomocam, m) {
         .def(py::init<np_array_t<float> &>())
         .def("shape", &tomocam::DArray<float>::shape)
         .def("to_numpy", &tomocam::DArray<float>::to_numpy);
+
+    // set gpu paramters
+    m.def("set_num_of_gpus", 
+            [](int num) {
+                tomocam::MachineConfig::getInstance().setNumOfGPUs(num);
+            }
+        );
+
+    m.def("set_num_of_streams",
+            [](int num) {
+                tomocam::MachineConfig::getInstance().setStreamsPerGPU(num);
+            }
+    );
+
+    m.def("set_slices_per_stream",
+            [](int num) {
+                tomocam::MachineConfig::getInstance().setSlicesPerStream(num);
+            }
+    );
     
     // radon transform
-    m.def("radon", &tomocam::radon);
+    m.def("radon", &radon_wrapper); 
 
     // iradon transform
-    m.def("iradon", &tomocam::iradon);
+    m.def("iradon", &iradon_wrapper);
 
     // gradients
-    m.def("gradients", &tomocam::gradient);
+    m.def("gradients", &gradient_wrapper);
 
     // add_tv
-    m.def("total_variation", &tomocam::add_total_var);
+    m.def("total_variation", &tv_wrapper);
 
     // caxpy
     m.def("axpy", &tomocam::axpy);
