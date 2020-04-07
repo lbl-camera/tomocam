2b0cfcac19769d4ddc9ccdd305df46bec7530612
Dinesh Kumar <dkumar@lbl.gov>
2020-04-06 21:18:15 -0700

    minor cosmetic changes

--- test/test.py
+++ test/test.py
@@ -3,13 +3,17 @@ import tomocam
 
 a = np.arange(20, dtype=np.float32)
 A = tomocam.DistArray(a)
+print('A = ')
 print(A.to_numpy())
 b = np.ones(20, dtype=np.float32)
 B = tomocam.DistArray(b)
+print('B = ')
 print(B.to_numpy())
 
+print('subtrac A from B')
 A -= B
 print(A.to_numpy())
+print('Shape of A')
 print(A.shape)
 d = A.to_numpy()
 print(d.shape)
