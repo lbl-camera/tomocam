e1cbec570d5e8497a9fc9eb2fd6b0dc73891c4db
Dinesh Kumar <dkumar@lbl.gov>
2020-05-18 18:13:44 -0700

    added axpy and nrom

--- tomocam/__init__.py
+++ tomocam/__init__.py
@@ -3,3 +3,9 @@
 from .darray import DistArray
 from .transform import radon, iradon
 from .modeling import calc_gradients, update_total_variation
+
+def axpy(alpha, y, x):
+    cTomocam.axpy(alpha, y.handle, x.handle)
+
+def norm(x):
+    return cTomocam.norm(x)
