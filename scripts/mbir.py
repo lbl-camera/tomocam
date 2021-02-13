52c17e8ae9bc29d8366b24c728f1a0d8432970ab
Dinesh Kumar <dkumar@lbl.gov>
2021-02-12 16:32:30 -0800

    Data test for mbir with simulated Shepp-Logan

--- scripts/mbir.py
+++ scripts/mbir.py
@@ -1,23 +1,16 @@
 import numpy as np
-import tomocam
+from tomocam import MBIR
+import tomopy
 
+import matplotlib.pyplot as plt
 
+center = 200.
+shepp = tomopy.misc.phantom.shepp2d(size = 400, dtype=np.float32)
+angles = np.linspace(0, np.pi, 360, dtype=np.float32)
+tomo = tomopy.sim.project.project(shepp, angles, center = center, pad = False, sinogram_order=True)
 
-# read sinogram data
-data = np.fromfile('/home/dkumar/Data/phantom/sino_00016.bin', dtype=np.float32)
-sino = tomocam.DistArray(data.reshape(20, 1536, 1280))
-model = tomocam.DistArray(np.ones((20, 1280, 1280), dtype=np.float32))
-angles = np.linspace(0, np.pi, sino.shape[1], dtype = np.float32)
+recon = MBIR(tomo.astype(np.float32), angles, center, num_iters = 150, smoothness = 10)
 
-n_iters = 10
-for i in range(n_iters):
-    gradients = model.copy()
-    print('copied ....')
-    tomocam.calc_gradients(gradients, sino, angles, center=640)
-    print('gradients done ....')
-    tomocam.update_total_variation(model, gradients)
-    print('tv done ....')
-    tomocam.axpy(-0.1, gradients, model)
-    print('update done ...')
-    error = 0.1 * gradients.norm()
-    print(error)
+plt.imshow(recon[0])
+plt.show()
+
