7cd9d4e0e67c671916ad0be641b0a3d56a8180f2
Dinesh Kumar <dkumar@lbl.gov>
2021-03-09 21:39:25 -0800

    plotting side-by-side

--- scripts/mbir.py
+++ scripts/mbir.py
@@ -9,8 +9,11 @@ shepp = tomopy.misc.phantom.shepp2d(size = 400, dtype=np.float32)
 angles = np.linspace(0, np.pi, 360, dtype=np.float32)
 tomo = tomopy.sim.project.project(shepp, angles, center = center, pad = False, sinogram_order=True)
 
-recon = MBIR(tomo.astype(np.float32), angles, center, num_iters = 150, smoothness = 10)
+recon = MBIR(tomo.astype(np.float32), angles, center, num_iters = 500, smoothness = 1)
 
+plt.subplot(121)
+plt.imshow(shepp[0])
+plt.subplot(122)
 plt.imshow(recon[0])
 plt.show()
 
