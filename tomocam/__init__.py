accb37c8c253a315d6433dc515368a6fba330a06
Dinesh Kumar <dkumar@lbl.gov>
2021-06-07 12:42:40 -0700

    removed darray and caxpy interfaces

--- tomocam/__init__.py
+++ tomocam/__init__.py
@@ -1,11 +1,7 @@
 
 
-from .darray import DistArray
 from .transform import radon, iradon
 from .modeling import calc_gradients, update_total_variation, MBIR
-
-def axpy(alpha, y, x):
-    cTomocam.axpy(alpha, y.handle, x.handle)
 
 def norm(x):
     return cTomocam.norm(x)
