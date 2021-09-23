32c8ef4f68000d60c98e8e5faac98bdec1ae0dec
Dinesh Kumar <dkumar@lbl.gov>
2021-09-22 18:15:55 -0700

    minor changes

--- test/test_mbir.cpp
+++ test/test_mbir.cpp
@@ -11,7 +11,7 @@
 
 const float PI = M_PI;
 const int MAX_ITERS = 150;
-const char * FILENAME = "/home/dkumar/data/phantom/phantom_00016/phantom_00016.h5";
+const char * FILENAME = "/home/dkumar/data/phantom_00017/phantom_00017.h5";
 const char * DATASET = "projs";
 const char * ANGLES = "angs";
 
@@ -20,7 +20,7 @@ int main(int argc, char **argv) {
     // read data
 	tomocam::H5Reader fp(FILENAME);
 	fp.setDataset(DATASET);
-	auto sino = fp.read_sinogram(16);
+	auto sino = fp.read_sinogram(64);
 	std::vector<float> angs = fp.read_angles(ANGLES);
 	float * angles = angs.data();
 
@@ -28,8 +28,9 @@ int main(int argc, char **argv) {
     tomocam::dim3_t d2(d1.x, d1.z, d1.z);
 	tomocam::DArray<float> model(d2);
 
+    int max_iters = 2;
     float center = 640;
-    float oversample = 1.5;
+    float oversample = 2;
     float sigma = 1;
     float p = 1.2;
 
@@ -37,12 +38,13 @@ int main(int argc, char **argv) {
         sigma = std::atof(argv[1]);
     }
 
-    std::cout << "Inpit size: (" << d1.x << ", " << d1.y << ", " << d1.z <<" )" << std::endl;
+    std::cout << "Input size: (" << d1.x << ", " << d1.y << ", " << d1.z <<" )" << std::endl;
 	std::cout << "Center: " << center << std::endl;
 	std::cout << "Oversampling: " << oversample << std::endl;
 	std::cout << "Smoothness: " << sigma << std::endl;
+	std::cout << "No. of iterations: " << max_iters << std::endl;
 
-	tomocam::mbir(sino, model, angles, center, 100, oversample, sigma, p);
+	tomocam::mbir(sino, model, angles, center, max_iters, oversample, sigma, p);
 
     std::fstream out("output.bin", std::fstream::out);
     out.write((char *) model.data(), model.size() * sizeof(float));
