53fea282d0c04dfe859a11962c76f993d1117528
Dinesh Kumar <dkumar@lbl.gov>
2021-09-22 18:13:01 -0700

    using tomocam.MBIR

--- scripts/test_mbir.py
+++ scripts/test_mbir.py
@@ -5,35 +5,16 @@ import tomocam
 import matplotlib.pyplot as plt
 
 
-filename = '/home/dkumar/Data/phantom/phantom_00016.h5'
+filename = '/home/dkumar/data/phantom_00017/phantom_00017.h5'
 h5fp = h5py.File(filename, 'r')
-
-data = h5fp['/projs']
-s = np.array(data[:,:16,:].transpose(1, 0, 2), copy=True)
+data = np.copy(h5fp['/projs'][:,:128,:])
+angs = np.copy(h5fp['/angs'])
 h5fp.close()
 
-nslc, nproj, nrow = s.shape
-print(s.shape)
-sino = tomocam.DistArray(s.copy())
-model = tomocam.DistArray(np.ones((nslc, nrow, nrow), dtype=np.float32))
-angs = np.linspace(0, np.pi, nproj, dtype=np.float32)
+print(data.shape)
+sino = np.transpose(data, [1, 0, 2])
 
-lam = 0.8
-prev_e = np.Infinity
+recon = tomocam.MBIR(sino, angs, 640, 10, over_sample=2, smoothness=1)
 
-for i in range(20):
-    grads = model.copy()
-    tomocam.calc_gradients(grads, sino, angs, center=640)
-    tomocam.update_total_variation(model, grads)
-    e = grads.norm() * lam
-    if e > prev_e:
-        lam *= 0.9
-    else: 
-        tomocam.axpy(-lam, grads, model)
-        prev_e = e
-        print(e)
-recon = model.to_numpy()
-
-for i in range(10):
-    plt.imshow(recon[i])
-    plt.show()
+plt.imshow(recon[0])
+plt.show()
