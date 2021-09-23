51cb2af17ff57d121edf9bee9150ec31a683157d
Dinesh Kumar <dkumar@lbl.gov>
2021-09-22 18:16:43 -0700

    using tomobank hdf5 phantom for testing

--- test/test_backward.cpp
+++ test/test_backward.cpp
@@ -4,39 +4,29 @@
 
 #include "dist_array.h"
 #include "tomocam.h"
+#include "reader.h"
 
-const char * FILENAME = "/home/dkumar/data/shepp_logan/sino400.bin";
+const char * FILENAME = "/home/dkumar/data/phantom_00017/phantom_00017.h5";
+const char * DATASET = "projs";
+const char * ANGLES = "angs";
 
 int main(int argc, char **argv) {
 
     // read data
-    const int dims[] = { 1, 400, 400 };
-    size_t size = dims[0] * dims[1] * dims[2];
-    float * data = new float[size];
+    tomocam::H5Reader fp(FILENAME);
+    fp.setDataset(DATASET);
+    auto sino = fp.read_sinogram(16);
+    auto angs = fp.read_angles(ANGLES);
+    float * theta = angs.data();
 
-	const int num_angles = 400;
-    float * angles = new float[num_angles];
-	for (int i = 0; i < num_angles; i++) 
-		angles[i] = M_PI * static_cast<float>(i) / static_cast<float>(num_angles-1);
-
-    std::ifstream fp(FILENAME);
-    if (! fp.is_open()) {
-        std::cerr << "error! unable to open data file." << std::endl;
-        exit(1);
-    }
-    fp.read((char *) data, sizeof(float) * size);
-
-    tomocam::dim3_t d1 = { dims[0], dims[1], dims[2] };
-    tomocam::DArray<float> sino(d1);
-    sino.init(data);
-    tomocam::dim3_t d2 = { dims[0], dims[2], dims[2] };
+    tomocam::dim3_t d1 = sino.dims();
+    tomocam::dim3_t d2 = {d1.x, d1.z, d1.z};
     tomocam::DArray<float> recn(d2);
     
-    //float center = 639.5;
-    float center = 200;
-    float oversample = 1.5;
+    float center = 640;
+    float oversample = 2;
 
-    tomocam::iradon(sino, recn, angles, center, oversample);
+    tomocam::iradon(sino, recn, theta, center, oversample);
   
     std::fstream out("goutput.bin", std::fstream::out);
     out.write((char *) recn.data(), recn.bytes());
