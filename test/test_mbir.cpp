e71446cc582efdf7f7ac47de98cb7e5996442112
Dinesh Kumar <dkumar@lbl.gov>
2023-09-22 06:07:24 -0700

    using json for input

--- test/test_mbir.cpp
+++ test/test_mbir.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-
+#include <nlohmann/json.hpp>
 
 #include "dist_array.h"
 #include "tomocam.h"
@@ -8,51 +8,69 @@
 #include "optimize.h"
 
 #include "reader.h"
+#include "writer.h"
 #include "timer.h"
 
+using json = nlohmann::json;
+
 const float PI = M_PI;
-const int MAX_ITERS = 150;
-const char * FILENAME = "/home/dkumar/data/tomo_00025/tomo25.h5";
-const char * DATASET = "projs";
-const char * ANGLES = "angs";
-const int NSLICES = 16;
+
 int main(int argc, char **argv) {
 
-    // read data
-	tomocam::H5Reader fp(FILENAME);
-	fp.setDataset(DATASET);
-	auto sino = fp.read_sinogram(NSLICES, 100);
-	std::vector<float> angs = fp.read_angles(ANGLES);
-	float * angles = angs.data();
-
-    tomocam::dim3_t d1 = sino.dims();
-    tomocam::dim3_t d2(d1.x, d1.z, d1.z);
-
-    int max_iters = 100;
-    float center = 952;
-    float oversample = 2;
-    float sigma = 10;
-    float p = 1.2;
 
     if (argc == 2) {
-        sigma = std::atof(argv[1]);
+        std::ifstream fin(argv[1]);
+        json cfg = json::parse(fin);
+
+        auto data = cfg["data"];
+        auto param = cfg["recon"];
+        
+        std::string filename = data["filename"];
+        std::string dataset = data["dataset"];
+        std::string theta = data["angles"];
+
+        int ibeg = 0;
+        int iend = 4;
+        if (!data["sinos"].is_null()) {
+            std::vector<int> sinos = data["sinos"];
+            ibeg = sinos[0]; 
+            iend = sinos[1];
+        }
+        int nslices = iend - ibeg;
+
+        // read data
+	    tomocam::H5Reader fp(filename.c_str());
+        fp.setDataset(dataset.c_str());
+	    auto sino = fp.read_sinogram(nslices, ibeg);
+        std::vector<float> angs = fp.read_angles(theta.c_str());
+        float * angles = angs.data();
+
+        tomocam::dim3_t d1 = sino.dims();
+        tomocam::dim3_t d2(d1.x, d1.z, d1.z);
+
+        int max_iters = param["num_iters"];
+        float center = data["axis"];
+        float smooth = param["smoothness"];
+        float sigma = 1.0 / smooth;
+
+        float p = 1.2;
+        float oversample = 2;
+
+
+        std::cout << "Input size: (" << d1.x << ", " << d1.y << ", " << d1.z <<" )" << std::endl;
+	    std::cout << "Center: " << center << std::endl;
+	    std::cout << "Oversampling: " << oversample << std::endl;
+	    std::cout << "1 / Smoothness: " << sigma << std::endl;
+	    std::cout << "No. of iterations: " << max_iters << std::endl;
+
+        Timer t;
+	    auto recon = tomocam::mbir(sino, angles, center, oversample, sigma, p, max_iters);
+        t.stop();
+        std::cout << "time taken(ms): " << t.millisec() << std::endl;
+
+        std::string outfile = param["output_filename"];
+        tomocam::H5Writer outf(outfile.c_str());
+        outf.write("recon", recon);
     }
-
-    std::cout << "Input size: (" << d1.x << ", " << d1.y << ", " << d1.z <<" )" << std::endl;
-	std::cout << "Center: " << center << std::endl;
-	std::cout << "Oversampling: " << oversample << std::endl;
-	std::cout << "Smoothness: " << sigma << std::endl;
-	std::cout << "No. of iterations: " << max_iters << std::endl;
-
-    // normalize
-    sino = sino * (1./ sino.max());
-    Timer t;
-	auto recon = tomocam::mbir(sino, angles, center, oversample, sigma, p, max_iters);
-    t.stop();
-    std::cout << "time taken(ms): " << t.millisec() << std::endl;
-
-    std::fstream out("recon.bin", std::ios::out | std::ios::binary);
-    out.write((char *) recon.data(), recon.size() * sizeof(float));
-
     return 0;
 }
