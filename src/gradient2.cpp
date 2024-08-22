5894aecf825d7ee0cd3ea228cbbf3da0e76560c9
Dinesh Kumar <dkumar@lbl.gov>
2024-08-22 10:59:28 -0700

    moved all to calculation inside scheduling, there only two lines.

--- src/gradient2.cpp
+++ src/gradient2.cpp
@@ -33,7 +33,7 @@
 namespace tomocam {
 
     template <typename T>
-    T gradient2_(Partition<T> f, Partition<T> sinoT, Partition<T> df,
+    void gradient2_(Partition<T> f, Partition<T> sinoT, Partition<T> df,
         const PointSpreadFunction<T> &psf, int device_id) {
 
         // set device
@@ -50,38 +50,33 @@ namespace tomocam {
         cudaStream_t copy_s;
         SAFE_CALL(cudaStreamCreate(&copy_s));
 
-        // vector to store partial function values
-        std::vector<T> pfunc(nparts, 0);
-
         // creater a scheduler, and assign work
         Scheduler<Partition<T>, DeviceArray<T>, DeviceArray<T>> s(p1, p2);
         while (s.has_work()) {
             auto work = s.get_work();
             if (work.has_value()) {
                 auto [idx, d_f, d_sinoT] = work.value();
-                auto [d_g, pf] = gradient2<T>(d_f, d_sinoT, psf, work_s);
+
+                // compute gradient
+                auto tmp = psf.convolve(d_f, work_s);
+                auto d_g = tmp.subtract(d_sinoT, work_s);
+
                 // synchronize work stream
                 SAFE_CALL(cudaStreamSynchronize(work_s));
 
                 // copy gradient to host
                 d_g.copy_to(p3[idx], copy_s);
-                pfunc[idx] = pf;
             }
         }
-
-        // accumulate partial function values
-        float partial_func = 0;
-        for (int i = 0; i < nparts; i++) { partial_func += pfunc[i]; }
 
         // synchronize and destroy copy stream
         SAFE_CALL(cudaStreamSynchronize(copy_s));
         SAFE_CALL(cudaStreamDestroy(copy_s));
-        return partial_func;
     }
 
     // Multi-GPU calll
     template <typename T>
-    std::tuple<DArray<T>, T> gradient2(DArray<T> &solution, DArray<T> &sinoT,
+    DArray<T> gradient2(DArray<T> &solution, DArray<T> &sinoT,
         const std::vector<PointSpreadFunction<T>> &psfs) {
 
         int nDevice = Machine::config.num_of_gpus();
@@ -94,13 +89,10 @@ namespace tomocam {
         auto p2 = create_partitions(sinoT, nDevice);
         auto p3 = create_partitions(gradient, nDevice);
 
-        // vecor to store partial function values
-        std::vector<T> pfunc(nDevice, 0);
-
         // launch all the available devices
-        //#pragma omp parallel for num_threads(nDevice)
+        #pragma omp parallel for num_threads(nDevice)
         for (int i = 0; i < nDevice; i++) {
-            pfunc[i] = gradient2_<T>(p1[i], p2[i], p3[i], psfs[i], i);
+            gradient2_<T>(p1[i], p2[i], p3[i], psfs[i], i);
         }
 
         // wait for devices to finish
@@ -109,18 +101,16 @@ namespace tomocam {
             SAFE_CALL(cudaSetDevice(i));
             SAFE_CALL(cudaDeviceSynchronize());
         }
-
-        float partial_func_val = 0;
-        for (int i = 0; i < nDevice; i++) { partial_func_val += pfunc[i]; }
-
-        return std::make_tuple(gradient, partial_func_val);
+        return gradient;
     }
 
     // specialization for float
-    template std::tuple<DArray<float>, float> gradient2<float>(DArray<float> &solution,
-        DArray<float> &sinoT, const std::vector<PointSpreadFunction<float>> &psfs);
+    template DArray<float> gradient2<float>(DArray<float> &solution,
+        DArray<float> &sinoT,
+        const std::vector<PointSpreadFunction<float>> &psfs);
     // specialization for double
-    template std::tuple<DArray<double>, double> gradient2<double>(DArray<double> &solution,
-        DArray<double> &sinoT, const std::vector<PointSpreadFunction<double>> &psfs);
+    template DArray<double> gradient2<double>(DArray<double> &solution,
+        DArray<double> &sinoT,
+        const std::vector<PointSpreadFunction<double>> &psfs);
 
 } // namespace tomocam
