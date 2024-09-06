b4d5f2a1e1038f401c43f093e197d1eb7db7a537
Dinesh Kumar <dkumar@lbl.gov>
2024-09-06 10:43:38 -0700

    minor changes

--- test/test_gradient.cpp
+++ test/test_gradient.cpp
@@ -15,8 +15,8 @@
 
 int main(int argc, char **argv) {
 
-    const int nprojs = 5;
-    const int npixel = 5;
+    const int nprojs = 200;
+    const int npixel = 2047;
     const int center = npixel / 2;
 
     auto random = []() { return static_cast<float>(rand()) / RAND_MAX; };
@@ -38,43 +38,43 @@ int main(int argc, char **argv) {
     x1.init(1.f);
     auto x2 = x1;
 
-    // gradient 1
-    auto t1 = tomocam::project(x1, angs, center);
-    auto g1 = tomocam::backproject(t1, angs, center);
+    // backproject sinogram
+    auto y = tomocam::backproject(sino, angs, center);
 
-    for (int i = 0; i < npixel; i++) {
-        for (int j = 0; j < npixel; j++) { std::cout << g1(0, i, j) << " "; }
-        std::cout << std::endl;
-    }
-    std::cout << "----------------" << std::endl;
+    // gradient 1
+    auto t1 = tomocam::project(x1, angs, center) - sino;
+    auto g1 = tomocam::backproject(t1, angs, center);
 
     // gradient 2
     // create NUFFT grids
     std::vector<tomocam::PointSpreadFunction<float>> psfs(4);
     for (int i = 0; i < 4; i++) {
-        tomocam::NUFFT::Grid<float> grid(sino.nrows(), sino.ncols(),
-            angs.data(), i);
+        tomocam::NUFFT::Grid<float> grid(nprojs, npixel, angs.data(), i);
         psfs[i] = tomocam::PointSpreadFunction<float>(grid);
     }
 
-    // compute gradient
-    auto y = tomocam::backproject(sino, angs, center);
-    auto g2 = tomocam::gradient2(x2, y, psfs);
-
-    for (int i = 0; i < npixel; i++) {
-        for (int j = 0; j < npixel; j++) { std::cout << g1(0, i, j) << " "; }
-        std::cout << std::endl;
+    /**
+    size_t free, total;
+    SAFE_CALL(cudaSetDevice(0));
+    tomocam::DeviceArray<float> dx2(x2.dims());
+    cudaMemcpy(dx2.dev_ptr(), x2.begin(), x2.bytes(), cudaMemcpyHostToDevice);
+    for (int i = 0; i < 100; i++) {
+        psfs[0].convolve(dx2);
+        SAFE_CALL(cudaMemGetInfo(&free, &total));
+        std::cout << "Free: " << free / 1024 / 1024 << " MB" << std::endl;
     }
-    std::cout << "----------------" << std::endl;
-    exit(1);
+    */
+
+    // compute gradient
+    auto g2 = tomocam::gradient2(x2, y, psfs);
 
     // write to HDF5
     tomocam::h5::Writer h5fw("gradient.h5");
-    h5fw.write("g1", t1);
+    h5fw.write("g1", g1);
     h5fw.write("g2", g2);
 
     // compare
     auto e = g1 - g2;
-    std::cout << "Error: " << e.norm() << std::endl;
+    std::cout << "Error: " << e.norm() / g1.norm() << std::endl;
     return 0;
 }
