480d790312f35c510f5e897f394a61202e72dd99
Dinesh Kumar <dkumar@lbl.gov>
2024-10-08 09:34:22 -0700

    Update test_toeplitz.cpp to use real_t type, increase nproj and ncols, and add gradient calculation using toeplitz matrix.

--- test/test_toeplitz.cpp
+++ test/test_toeplitz.cpp
@@ -11,6 +11,14 @@
 #include "toeplitz.h"
 #include "tomocam.h"
 
+#define USE_DOUBLE1
+
+#ifdef USE_DOUBLE
+typedef double real_t;
+#else
+typedef float real_t;
+#endif
+
 template <typename T>
 T random() {
     return static_cast<T>(rand()) / static_cast<T>(RAND_MAX);
@@ -19,33 +27,50 @@ T random() {
 int main(int argc, char **argv) {
 
     // read data
-    int nproj = 5;
-    int ncols = 5;
+    int nproj = 360;
+    int ncols = 511;
+    real_t center = static_cast<real_t>(ncols) / 2;
 
     // create a hdf5 writer
-    tomocam::h5::H5Writer fp("test_toeplitz.h5");
-
+    tomocam::h5::Writer fp("test_toeplitz.h5");
     tomocam::dim3_t dim1 = {1, nproj, ncols};
     tomocam::dim3_t dim2 = {1, ncols, ncols};
 
-    // set seed
-    srand(101);
-
     // generate random data
-    tomocam::DArray<float> f1(dim1);
-    for (int i = 0; i < f1.size(); i++) f1[i] = random<float>();
+    tomocam::DArray<real_t> y(dim1);
+    for (int i = 0; i < y.size(); i++) y[i] = random<real_t>();
 
     // initialize solution
-    tomocam::DArray<float> f(dim2);
-    for (int i = 0; i < f.size(); i++) f[i] = 1.0; // random<float>();
+    tomocam::DArray<real_t> f(dim2);
+    for (int i = 0; i < f.size(); i++) f[i] = 1.0;
 
-    // angles 0 - 180
-    std::vector<float> theta(nproj);
+    // angles 0 - 180 degrees
+    std::vector<real_t> theta(nproj);
     for (int i = 0; i < nproj; i++) theta[i] = i * M_PI / nproj;
 
-    // calculate backprojection of data
-    auto yT = tomocam::backproject(f1, theta);
+    // create a nugrid and psfs
+    int ndevices = 4;
+    std::vector<tomocam::NUFFT::Grid<real_t>> nugrids(ndevices);
+    for (int i = 0; i < ndevices; i++)
+        nugrids[i] =
+            tomocam::NUFFT::Grid<real_t>(nproj, ncols, theta.data(), i);
 
+    std::vector<tomocam::PointSpreadFunction<real_t>> psfs(ndevices);
+    for (int i = 0; i < ndevices; i++)
+        psfs[i] = tomocam::PointSpreadFunction<real_t>(nugrids[i]);
+
+    // calculate backprojection of data
+    auto yT = tomocam::backproject(y, theta, center);
+
+    // calculate classical gradient
+    auto g1 = tomocam::gradient(f, y, nugrids, center);
+
+    // calculate gradient using toeplitz matrix
+    auto g2 = tomocam::gradient2(f, yT, psfs);
+
+    // compare the two gradients
+    auto err = std::sqrt((g1 - g2).norm()) / std::sqrt(g1.norm());
+    std::cout << "Relative error: " << err << std::endl;
     return 0;
 }
 
