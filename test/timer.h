12aede304b78352b10812177d11ec1b4f5ba6046
Dinesh Kumar <dkumar@lbl.gov>
2024-10-26 09:26:11 -0700

    removing old filenames from the repo

--- test/timer.h
+++ test/timer.h
@@ -1,28 +1,51 @@
 
 #include <chrono>
 #include <iostream>
+#include <random>
 
 #ifndef TIMEIT_H
 
 class Timer {
     private:
-      std::chrono::high_resolution_clock::time_point start_;
-      std::chrono::milliseconds duration_;
+        std::chrono::high_resolution_clock::time_point start_;
+        std::chrono::milliseconds duration_;
 
     public:
-      Timer() : duration_(0) {}
+        Timer() : duration_(0) {}
 
-      void start() { start_ = std::chrono::high_resolution_clock::now(); }
+        void start() {
+            start_ = std::chrono::high_resolution_clock::now();
+        }
 
-      void stop() {
-          auto end = std::chrono::high_resolution_clock::now();
-          duration_ += std::chrono::duration_cast<std::chrono::milliseconds>(
-              end - start_);
-      }
+        void stop() {
+            auto end = std::chrono::high_resolution_clock::now();
+            duration_ += std::chrono::duration_cast<std::chrono::milliseconds>(
+                end - start_);
+        }
 
-      void reset() { duration_ = std::chrono::milliseconds(0); }
-      int ms() { return duration_.count(); }
+        void reset() {
+            duration_ = std::chrono::milliseconds(0);
+        }
+        int ms() {
+            return duration_.count();
+        }
 
 };
 
+class NPRandom {
+    private:
+        std::mt19937 gen_;
+
+    public:
+        NPRandom() {
+            gen_ = std::mt19937(5489u);
+        }
+
+        template <typename T>
+        T rand() {
+            int a = gen_() >> 5;
+            int b = gen_() >> 6;
+            return static_cast<T>((a * 67108864.0 + b) / 9007199254740992.0);
+        }
+};
 #endif // TIMEIT_H
