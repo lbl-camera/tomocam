cfe626e3fd9ef2c1ee0bd72a8171b87d3852c6b5
Dinesh Kumar <dkumar@lbl.gov>
2024-10-27 09:53:27 -0700

    Added wrapper around FIND_HDF5 to avoid pain and failure

---
+++ cmake/Findhdf5.cmake
@@ -0,0 +1,29 @@
+include(FindPackageHandleStandardArgs)
+
+set(hdf5_SEARCH_PATHS ${HDF5_DIR})
+
+find_path(hdf5_INCLUDE_DIR
+    NAMES hdf5.h
+    HINTS $ENV{HDF5_DIR}
+    PATH_SUFFIXES include
+    PATHS ${hdf5_SEARCH_PATHS}
+)
+
+find_library(hdf5_LIBRARY
+    NAMES hdf5
+    HINTS $ENV{HDF5_DIR}
+    PATH_SUFFIXES lib64 lib
+    PATHS ${hdf5_SEARCH_PATHS}
+)
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(hdf5
+    REQUIRED_VARS 
+    hdf5_LIBRARY 
+    hdf5_INCLUDE_DIR
+) 
+
+if(hdf5_LIBRARY AND hdf5_INCLUDE_DIR)
+    mark_as_advanced(hdf5_FOUND YES)
+    mark_as_advanced(hdf5_LIBRARY)
+    mark_as_advanced(hdf5_INCLUDE_DIR)
+endif()
