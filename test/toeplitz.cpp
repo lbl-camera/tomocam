93e5a7a0629b2ad435e8c206eb11b0c29c17e11d
Dinesh Kumar <dkumar@lbl.gov>
2025-04-22 13:23:08 -0700

    minor trivial not so important changes

--- test/toeplitz.cpp
+++ test/toeplitz.cpp
@@ -5,13 +5,15 @@
 #include <random>
 
 #include <cuda_runtime.h>
-
+#include "timer.h"
 #include "dist_array.h"
 #include "hdf5/writer.h"
 #include "toeplitz.h"
-#include "tomocam.h"
 
-#define USE_DOUBLE1
+#include "tomocam.h"
+#include "timer.h"
+
+#define USE_DOUBLE
 
 #ifdef USE_DOUBLE
 typedef double real_t;
@@ -19,16 +21,11 @@ typedef double real_t;
 typedef float real_t;
 #endif
 
-template <typename T>
-T random() {
-    return static_cast<T>(rand()) / static_cast<T>(RAND_MAX);
-}
-
 int main(int argc, char **argv) {
 
     // read data
     int nproj = 360;
-    int ncols = 511;
+    int ncols = 2047;
     real_t center = static_cast<real_t>(ncols) / 2;
 
     // create a hdf5 writer
@@ -37,8 +34,9 @@ int main(int argc, char **argv) {
     tomocam::dim3_t dim2 = {1, ncols, ncols};
 
     // generate random data
+    auto rng = NPRandom();
     tomocam::DArray<real_t> y(dim1);
-    for (int i = 0; i < y.size(); i++) y[i] = random<real_t>();
+    for (int i = 0; i < y.size(); i++) y[i] = rng.rand<real_t>();
 
     // initialize solution
     tomocam::DArray<real_t> f(dim2);
@@ -52,8 +50,7 @@ int main(int argc, char **argv) {
     int ndevices = 4;
     std::vector<tomocam::NUFFT::Grid<real_t>> nugrids(ndevices);
     for (int i = 0; i < ndevices; i++)
-        nugrids[i] =
-            tomocam::NUFFT::Grid<real_t>(nproj, ncols, theta.data(), i);
+        nugrids[i] = tomocam::NUFFT::Grid<real_t>(nproj, ncols, theta.data(), i);
 
     std::vector<tomocam::PointSpreadFunction<real_t>> psfs(ndevices);
     for (int i = 0; i < ndevices; i++)
@@ -62,11 +59,26 @@ int main(int argc, char **argv) {
     // calculate backprojection of data
     auto yT = tomocam::backproject(y, theta, center);
 
+    Timer t1;
+    t1.start();
+
     // calculate classical gradient
-    auto g1 = tomocam::gradient(f, y, nugrids, center);
+    auto g1 = tomocam::gradient(f, yT, nugrids, center);
+
+    t1.stop();
 
     // calculate gradient using toeplitz matrix
+    Timer t2;
+    t2.start();
     auto g2 = tomocam::gradient2(f, yT, psfs);
+    t2.stop();
+
+    std::cout << "g1.norm(): " << g1.norm() << std::endl;
+    std::cout << "g2.norm(): " << g2.norm() << std::endl;
+
+    fprintf(stdout, "Time taken(ms): regular method: %d\n", t1.ms());
+    fprintf(stdout, "Time taken(ms): toeplitz method: %d\n", t2.ms());
+
 
     // compare the two gradients
     auto err = std::sqrt((g1 - g2).norm()) / std::sqrt(g1.norm());
