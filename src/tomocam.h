6ed8e70d77045f17321630c2b1259b4388969039
Dinesh Kumar <dkumar@lbl.gov>
2025-04-22 10:58:56 -0700

    updated inline documentation

--- src/tomocam.h
+++ src/tomocam.h
@@ -81,7 +81,7 @@ namespace tomocam {
 
     template <typename T>
     DArray<T> gradient2(DArray<T> &, DArray<T> &,
-        const std::vector<PointSpreadFunction<T>> &);
+        std::vector<PointSpreadFunction<T>> &);
 
     /**
      * @brief Compute the value of the objective function, given current
@@ -122,8 +122,20 @@ namespace tomocam {
     template <typename T>
     void add_total_var(DArray<T> &, DArray<T> &, float, float);
 
+    /** 
+     * @brief Compute the MBIR reconstruction using Toeplitz matrix.
+     *
+     * @param initial guess
+     * @param sinogram The sinogram to reconstruct.
+     * @param angles The angles of the sinogram.
+     * @param center The center of rotation.
+     * @param num_iter The number of iterations.
+     * @param sigma The regularization parameter.
+     * @param tolerance The stopping criterion.
+     * @param xtol The tolerance for the solution.
+     */
     template <typename T>
-    DArray<T> mbir2(DArray<T> &, std::vector<T>, T, T, T, int, T, T, T);
+    DArray<T> mbir2(DArray<T> &, const DArray<T> &, std::vector<T>, T, int, T, T, T);
 
     /**
      * @brief Compute the MBIR reconstruction.
