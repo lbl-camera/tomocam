a8a818c0a24d825e3c8a422af0b6fc1c10acb1f4
Dinesh Kumar <dkumar@lbl.gov>
2025-05-24 07:29:17 -0700

    Remove unused 'center' parameter from function_value and funcval functions

--- src/error.cpp
+++ src/error.cpp
@@ -33,7 +33,7 @@ namespace tomocam {
 
     template <typename T>
     T funcval(Partition<T> recon, Partition<T> sino,
-        const NUFFT::Grid<T> &nugrid, T center, int device_id) {
+        const NUFFT::Grid<T> &nugrid, int device_id) {
 
         // set device
         cudaSetDevice(device_id);
@@ -52,7 +52,7 @@ namespace tomocam {
             auto work = scheduler.get_work();
             if (work.has_value()) {
                 auto[idx, d_recon, d_sino] = work.value();
-                auto t1 = project(d_recon, nugrid, center);
+                auto t1 = project(d_recon, nugrid);
                 auto t2 = t1 - d_sino;
                 sum += t2.dot(t2);
             }
@@ -63,7 +63,7 @@ namespace tomocam {
     // Multi-GPU calll
     template <typename T>
     T function_value(DArray<T> &recon, DArray<T> &sino,
-        const std::vector<NUFFT::Grid<T>> &nugrids, T center) {
+        const std::vector<NUFFT::Grid<T>> &nugrids) {
 
         int nDevice = Machine::config.num_of_gpus();
         if (nDevice > recon.nslices()) nDevice = recon.nslices();
@@ -75,7 +75,7 @@ namespace tomocam {
         std::vector<T> retval(nDevice);
         #pragma omp parallel for num_threads(nDevice)
         for (int i = 0; i < nDevice; i++)
-            retval[i] = funcval(p1[i], p2[i], nugrids[i], center, i);
+            retval[i] = funcval(p1[i], p2[i], nugrids[i], i);
 
         // wait for devices to finish
         T fval = 0;
@@ -90,7 +90,7 @@ namespace tomocam {
 
     // explicit instantiation
     template float function_value(DArray<float> &, DArray<float> &,
-        const std::vector<NUFFT::Grid<float>> &, float);
+        const std::vector<NUFFT::Grid<float>> &);
     template double function_value(DArray<double> &, DArray<double> &,
-        const std::vector<NUFFT::Grid<double>> &, double);
+        const std::vector<NUFFT::Grid<double>> &);
 } // namespace tomocam
