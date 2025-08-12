6490a4ced53d2aae70a1d2f0d2acb2e3e75fca8d
Dinesh Kumar <dkumar@lbl.gov>
2025-08-12 16:01:03 -0700

    Remove unified memory checks and add device synchronization barrier to MachineConfig

--- src/machine.h
+++ src/machine.h
@@ -30,27 +30,14 @@ namespace tomocam {
     class MachineConfig {
       private:
         int ndevice_;
-        bool unified_;
         int slcsPerStream_;
 
       public:
         MachineConfig() {
-
             cudaGetDeviceCount(&ndevice_);
             #ifdef DEBUG
             ndevice_ = 1; // for debugging
             #endif
-
-            //check avilable GPU for managed memory access
-            std::vector<int> devices;
-            for (int i = 0; i < ndevice_; i++) {
-                int result = -1;
-                cudaDeviceGetAttribute(&result, cudaDevAttrConcurrentManagedAccess, i);
-                if (result != 0) devices.push_back(i);
-            }
-            if (devices.empty()) unified_ = true;
-            else
-                unified_ = false;
             slcsPerStream_ = 4; // slices
         }
 
@@ -68,7 +55,6 @@ namespace tomocam {
 
         // getters
         int num_of_gpus() const { return ndevice_; }
-        int is_unified() const { return unified_; }
         int slicesPerStream() const { return slcsPerStream_; }
 
         /* calculate number of sub-partitions, based on free memory */
@@ -87,6 +73,13 @@ namespace tomocam {
             int n_partitions = dims.x / slcs;
             if (dims.x % slcs > 0) n_partitions++;
             return n_partitions;
+        }
+
+        void barrier() {
+            for (int i = 0; i < ndevice_; i++) {
+                cudaSetDevice(i);
+                cudaDeviceSynchronize();
+            }
         }
     };
 
