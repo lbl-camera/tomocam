58ba2842c146c49ce64586245eb9d922198db5c8
Dinesh Kumar <dkumar@lbl.gov>
2025-10-17 16:51:27 +0000

    - Update mbir2 to accept initial guess x0 as input - Add sinogram normalization and x0 padding - Adjust optimizer parameters and error calculation for multiproc - Update explicit template instantiations - Minor fixes to device selection and postprocessing

--- src/mbir2.cpp
+++ src/mbir2.cpp
@@ -25,6 +25,7 @@
 #include <pybind11/pybind11.h>
 
 #include "dist_array.h"
+#include "padding.h"
 #include "optimize.h"
 #include "tomocam.h"
 #include "machine.h"
@@ -33,39 +34,50 @@
 namespace tomocam {
 
     template <typename T>
-    DArray<T> mbir2(DArray<T> &sino, std::vector<T> angles, T center, T sigma,
-        T p, int num_iters, T step_size, T tol, T penalty) {
+    DArray<T> mbir2(DArray<T> &x0, DArray<T> &sino, std::vector<T> angles, 
+            T center, int num_iters, T sigma, T tol, T xtol) {
 
-        // pad and shift sinogram
+        // preprocess
         int nrays = sino.ncols();
-        sino = preproc(sino, center);
+        auto sino2 = preproc(sino, center);
+
+        // normalize
+        auto maxv = sino2.max();
+        #ifdef MULTIPROC
+        maxv = multiproc::mp.MaxReduce(maxv);
+        #endif
+        sino2 /= maxv;
+
+        // pad x0 if needed
+        int npad = sino2.ncols() - x0.ncols();
+        if (npad > 0) x0 = pad2d(x0, npad, PadType::SYMMETRIC);
+        if (npad < 0) throw std::runtime_error("x0 is too large for the sinogram");
 
         // recon dimensions
-        int nslcs = sino.nslices();
-        int nproj = sino.nrows();
-        int ncols = sino.ncols();
+        int nslcs = sino2.nslices();
+        int nproj = sino2.nrows();
+        int ncols = sino2.ncols();
 
-        dim3_t dims(nslcs, ncols, ncols);
-        DArray<T> sinoT = backproject(sino, angles, center);
+        // backproject sinogram
+        auto sinoT = backproject(sino2, angles, center);
 
         // sinogram dot sinogram
-        T sino_norm = sino.norm();
-
-        // initialize x0
-        DArray<T> x0(dims);
-        x0.init(1);
+        T sino_norm = sino2.norm();
 
         // number of gpus available
         int ndevice = Machine::config.num_of_gpus();
-        if (ndevice > nslcs) { ndevice = 1; }
+        if (ndevice > nslcs) { ndevice = nslcs; }
 
         // calculate point-spread function for each device
+        int current_dev = 0;
+        SAFE_CALL(cudaGetDevice(&current_dev));
         std::vector<PointSpreadFunction<T>> psfs(ndevice);
         for (int dev_id = 0; dev_id < ndevice; dev_id++) {
             SAFE_CALL(cudaSetDevice(dev_id));
             auto nugrid = NUFFT::Grid(nproj, ncols, angles.data(), dev_id);
             psfs[dev_id] = PointSpreadFunction(nugrid);
         }
+        SAFE_CALL(cudaSetDevice(current_dev));
 
         // compute Lipschitz constant
         DArray<T> xtmp(dim3_t(1, ncols, ncols));
@@ -75,17 +87,27 @@ namespace tomocam {
         auto g = gradient2(xtmp, ytmp, psfs);
         gpu::add_tv_hessian(g, sigma);
         T L = g.max();
-        step_size = step_size / L;
+
+        #ifdef MULTIPROC
+        L = multiproc::mp.MaxReduce(L);
+        #endif
+        T step_size = 1 / L;
+        if (step_size > 1) { step_size = 1; }
+        T p = 1.2;
 
         // create callable functions for optimization
-        auto calc_gradient = [&sinoT, &psfs, p, sigma](DArray<T> &x) -> DArray<T> {
+        auto calc_gradient = [&sinoT, &psfs, center, sigma, p](DArray<T> &x) -> DArray<T> {
             auto g = gradient2(x, sinoT, psfs);
-            add_total_var(x, g, p, sigma);
+            add_total_var(x, g, sigma, p);
             return g;
         };
 
         auto calc_error = [&sinoT, &psfs, sino_norm](DArray<T> &x) -> T {
-            return function_value2(x, sinoT, psfs, sino_norm);
+            T e = function_value2(x, sinoT, psfs, sino_norm);
+            #ifdef MULTIPROC
+            e = multiproc::mp.SumReduce(e);
+            #endif
+            return e;
         };
 
         // create optimizer
@@ -93,13 +115,16 @@ namespace tomocam {
             calc_gradient, calc_error);
 
         // run optimization
-        auto rec = opt.run2(x0, num_iters, step_size, tol, tol);
-        return postproc(rec, nrays);
+        auto recon = opt.run2(x0, num_iters, step_size, tol, xtol);
+
+        // postprocess
+        return postproc(recon, nrays);
+
     }
 
     // explicit instantiation
-    template DArray<float> mbir2(DArray<float> &, std::vector<float>, float,
-        float, float, int, float, float, float);
-    template DArray<double> mbir2(DArray<double> &, std::vector<double>, double,
-        double, double, int, double, double, double);
+    template DArray<float> mbir2(DArray<float> &, DArray<float> &, std::vector<float>, 
+            float, int, float, float, float);
+    template DArray<double> mbir2(DArray<double> &, DArray<double> &, std::vector<double>, 
+            double, int, double, double, double);
 } // namespace tomocam
